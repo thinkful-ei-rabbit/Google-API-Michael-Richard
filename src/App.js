59c0cfef2d55b1685d1d1b83e37c6b37ea1c26eb
Michael Sliger <michaelj.sliger@gmail.com>
2020-08-31 09:23:03 -0600

    initial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Form from './Form';
+import Booklist from './Booklist'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <Booklist />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Booklist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Booklist extends React.Component {
+    render() {
+        return (
+            <div>
+                Example List
+            </div>
+        )
+    }
+}
+
+export default Booklist;
